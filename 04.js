bcba5c5dc49d3bd5c5736d4b5f1d89f445106ee9
songmiO <dhthd1060@gmail.com>
2021-11-25 23:56:31 +0900

    Modify : Delete Command.

--- 04.js
+++ 04.js
@@ -1,12 +1,10 @@
-# 04-01
+// # 04-01
 
-```javascript
 10 + 20;
-```
 
-# 04-02
 
-```javascript
+// # 04-02
+
 // 변수는 하나의 값을 저장하기 위한 수단이다.
 var userId = 1;
 var userName = 'Lee';
@@ -18,7 +16,7 @@ var users = [
   { id: 1, name: 'Lee' },
   { id: 2, name: 'Kim' },
 ];
-```
+
 
 # 04-03
 
@@ -26,36 +24,32 @@ var users = [
 var result = 10 + 20;
 ```
 
-# 04-04
+// # 04-04
 
-```javascript
 var score; // 변수 선언(변수 선언문)
-```
 
-# 04-05
 
-```javascript
+// # 04-05
+
 console.log(score); // undefined
 
 var score; // 변수 선언문
-```
 
-# 04-06
 
-```javascript
+// # 04-06
+
 var score; // 변수 선언
 score = 80; // 값의 할당
-```
 
-# 04-07
 
-```javascript
+// # 04-07
+
+
 var score = 80; // 변수 선언과 값의 할당
-```
 
-# 04-08
 
-```javascript
+// # 04-08
+
 console.log(score); // undefined
 
 var score; // ① 변수 선언
@@ -63,83 +57,65 @@ var score; // ① 변수 선언
 score = 80; // ② 값의 할당
 
 console.log(score); // 80
-```
 
-# 04-09
+// # 04-09
 
-```javascript
 console.log(score); // undefined
 
 var score = 80; // 변수 선언과 값의 할당
 
 console.log(score); // 80
-```
 
-# 04-10
+// # 04-10
 
-```javascript
 console.log(score); // undefined
 
 score = 80; // 값의 할당
 var score; // 변수 선언
 
 console.log(score); // ??
-```
 
-# 04-11
+// # 04-11
 
-```javascript
 var score = 80; // 변수 선언과 값의 할당
 score = 90; // 값의 재할당
-```
 
-# 04-12
+// # 04-12
 
-```javascript
 var person, $elem, _name, first_name, val1;
-```
 
-# 04-13
+// # 04-13
 
-```javascript
 var 이름, なまえ;
-```
 
-# 04-14
+// # 04-14
 
-```javascript
 var first-name; // SyntaxError: Unexpected token –
 var 1st;        // SyntaxError: Invalid or unexpected token
 var this;       // SyntaxError: Unexpected token this
-```
 
-# 04-15
+// # 04-15
 
-```javascript
 var firstname;
 var firstName;
 var FIRSTNAME;
-```
 
-# 04-16
 
-```javascript
+// # 04-16
+
 var x = 3; // NG. x 변수가 의미하는 바를 알 수 없다.
 var score = 100; // OK. score 변수는 점수를 의미한다.
-```
 
-# 04-17
+// # 04-17
 
-```javascript
 // 경과 시간. 단위는 날짜다
 var d; // NG
 
 var elapsedTimeInDays; // OK
-```
 
-# 04-18
 
-```javascript
+// # 04-18
+
 // 카멜 케이스 (camelCase)
 var firstName;
 
@@ -153,4 +129,3 @@ var FirstName;
 var strFirstName; // type + identifier
 var $elem = document.getElementById('myId'); // DOM 노드
 var observable$ = fromEvent(document, 'click'); // RxJS 옵저버블
-```
